edbdc3315dff4625984be85d3aad2acca58148d9
achesak <achesak@yahoo.com>
2014-01-05 11:14:57 -0600

    Well maybe not.

--- app/scripts/view.js
+++ app/scripts/view.js
@@ -13,53 +13,44 @@ var viewData = function() {
         var data = [];
     } else {
         var data = dataStorage.getJSON("weatherData");
-    }
+    }        
     
-    // If there is no data, don't display it.
-    if (data.length == 0) {
+    // Clear any existing data. (Is this step necessary?)
+    $("#listview").empty();
         
-        $("#content").html("<p>There's nothing here!</p><p>Tap the Add button in the bottom left corner to get started.</p>");
-    
-    // If there is data, display it.
-    } else {
+    // Loop through all the data items and add them.
+    for (var i = 0; i < data.length; i++ ) {
         
-        $("#listview").empty();
+        // Create the new item.
+        var item = $("<li/>").append($("<h3/>", {text: data[i][0]}));
+        var para = $("<p/>");
         
-        // Loop through all the data items and add them.
-        for (var i = 0; i < data.length; i++ ) {
-            
-            // Create the new item.
-            var item = $("<li/>").append($("<h3/>", {text: data[i][0]}));
-            var para = $("<p/>");
-            
-            // Create the data string.
-            var dataStr = "Temperature (°C): " + (Math.round(parseFloat(data[i][1]) * 100) / 100) + "<br />";
-            if (data[i][2] == "None") {
-                dataStr += "Precipitation (cm): None<br />";
-            } else {
-                split2 = data[i][2].split(" ");
-                dataStr += "Precipitation (cm): " + (Math.round(parseFloat(split2[0]) * 100) / 100) + " " + split2[1] + "<br />";
-            }
-            if (data[i][3] == "None") {
-                dataStr += "Wind (kph): None<br />";
-            } else {
-                split3 = data[i][3].split(" ");
-                dataStr += "Wind (kph): " + (Math.round(parseFloat(split3[0]) * 100) / 100) + " " + split3[1] + "<br />";
-            }
-            dataStr += "Humidity (%): " + data[i][4] + "<br />";
-            split5 = data[i][5].split(" ");
-            dataStr += "Air pressure (hPa): " + (Math.round(parseFloat(split5[0]) * 100) / 100) + " " + split5[1] + "<br />";
-            dataStr += "Cloud cover: " + data[i][6];
-            
-            // Add the data string.
-            para.html(dataStr);
-            // Append the data.
-            item.append(para);
-            // Insert the data.
-            $("#listview").append(item);
-            $("#listview").listview("refresh");
+        // Create the data string.
+        var dataStr = "Temperature (°C): " + (Math.round(parseFloat(data[i][1]) * 100) / 100) + "<br />";
+        if (data[i][2] == "None") {
+            dataStr += "Precipitation (cm): None<br />";
+        } else {
+            split2 = data[i][2].split(" ");
+            dataStr += "Precipitation (cm): " + (Math.round(parseFloat(split2[0]) * 100) / 100) + " " + split2[1] + "<br />";
         }
+        if (data[i][3] == "None") {
+            dataStr += "Wind (kph): None<br />";
+        } else {
+            split3 = data[i][3].split(" ");
+            dataStr += "Wind (kph): " + (Math.round(parseFloat(split3[0]) * 100) / 100) + " " + split3[1] + "<br />";
+        }
+        dataStr += "Humidity (%): " + data[i][4] + "<br />";
+        split5 = data[i][5].split(" ");
+        dataStr += "Air pressure (hPa): " + (Math.round(parseFloat(split5[0]) * 100) / 100) + " " + split5[1] + "<br />";
+        dataStr += "Cloud cover: " + data[i][6];
+        
+        // Add the data string.
+        para.html(dataStr);
+        // Append the data.
+        item.append(para);
+        // Insert the data.
+        $("#listview").append(item);
+        $("#listview").listview("refresh");
     }
-            
         
 };
